cab42d6b652df1bb32c47ebe5706e5935acab25f
wenweii <weiwei86122538@gmail.com>
2018-12-04 00:19:08 +0800

    1204

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -23,6 +23,28 @@ import sys
 
 import Adafruit_DHT
 
+import time
+import httplib, urllib
+import json
+deviceId = "DPuczJkk"
+deviceKey = "TDBIjmYg0Ta0vMmZ"
+def post_to_mcs(payload): 
+	headers = {"Content-type": "application/json", "deviceKey": deviceKey} 
+	not_connected = 1 
+	while (not_connected):
+		try:
+			conn = http.client.HTTPConnection("api.mediatek.com:80")
+			conn.connect() 
+			not_connected = 0 
+		except (http.client.HTTPException, socket.error) as ex: 
+			print ("Error: %s")
+ 			time.sleep(10)
+			 # sleep 10 seconds 
+	conn.request("POST", "/mcs/v2/devices/" + deviceId + "/datapoints", json.dumps(payload), headers) 
+	response = conn.getresponse() 
+	print( response.status, response.reason, json.dumps(payload), time.strftime("%c")) 
+	data = response.read() 
+	conn.close() 
 
 # Parse command line parameters.
 sensor_args = { '11': Adafruit_DHT.DHT11,
@@ -38,7 +60,6 @@ else:
 
 # Try to grab a sensor reading.  Use the read_retry method which will retry up
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
 # Un-comment the line below to convert the temperature to Fahrenheit.
 # temperature = temperature * 9/5.0 + 32
@@ -47,8 +68,10 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 # the results will be null (because Linux can't
 # guarantee the timing of calls to read the sensor).
 # If this happens try again!
-if humidity is not None and temperature is not None:
-    print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-else:
-    print('Failed to get reading. Try again!')
-    sys.exit(1)
+while True:
+	h0, t0 = Adafruit_DHT.read_retry(sensor, pin)
+	if h0 is not None and t0 is not None:
+		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+	else:
+		print('Failed to get reading. Try again!')
+		sys.exit(1)
